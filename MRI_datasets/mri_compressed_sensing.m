84fa899414dadd6e00af0c5bc93cf93df4e8d9d5
Hedda Fregne <heddafregne@Heddas-MacBook-Air.local>
2024-10-08 14:44:50 +0200

    moving functions to end of file

--- MRI_datasets/mri_compressed_sensing.m
+++ MRI_datasets/mri_compressed_sensing.m
@@ -7,7 +7,102 @@ load('MRI_datasets/Slice4/BadData/slice4_channel1.mat');
 load('MRI_datasets/Slice4/BadData/slice4_channel2.mat');
 load('MRI_datasets/Slice4/BadData/slice4_channel3.mat');
 
- function v=shuffle(v)
+ 
+
+ C= spiral_matrix(200,100,10,200, 5);
+close all
+figure(1); 
+imagesc(C);
+
+
+%%
+
+n = 512*128; % dimension of s
+p = round(n/100);
+kspace = reshape(slice4_channel1_goodData, n, 1);
+%C = sparse_matrix(p,n,p*500);
+%C = center_matrix(p,n);
+C = radial_matrix(p,n,30,50);
+%C = spiral_matrix(p,n,50,100000,15);
+y = C*kspace;
+
+close all
+axis image, 
+figure(1); 
+imagesc(C);
+
+%%
+% L1 minimum norm solution s_L1
+C = double(C);
+y = double(y);
+cvx_begin;
+    variable s_L1(n) complex;
+    minimize(norm(s_L1,1));
+    subject to 
+        norm(C*s_L1 - y, 2) < 10;
+cvx_end;
+
+%s_L2 = pinv(Theta)*y; % L2 minimum norm solution
+
+% Example of Steve Brunton code
+% https://stackoverflow.com/questions/66510827/compressed-sensing-why-does-my-convex-solver-fail-when-i-give-it-more-sample
+
+%%
+% IFFT of k-space data
+%channel 1 (replace "slice1_channel1_goodData" with
+%slice1_channel1_badData) for bad images
+s_L1 = reshape(s_L1, 512, 128)
+Data_img(:,:,1) = ifftshift(ifft2(s_L1),1);
+
+% clear compensation, preparation, based on fourier transformed blinked 
+% k-space data (Data_raw)
+clear_comp = linspace(10,0.1,size(Data_img,2)).^2; 
+clear_matrix = repmat(clear_comp,[size(Data_img,1) 1]);
+
+% combine 3 channels sum of squares and add clear compensation
+eye_raw  = sqrt( abs(squeeze(Data_img(:,:,1))).^2).* clear_matrix;  
+    
+% crop images because we are only interested in eye. Make it square 
+% 128 x 128
+crop_x = [128 + 60 : 348 - 33]; % crop coordinates
+eye_raw = eye_raw(crop_x, :);
+
+% Visualize the images. 
+
+%image
+eye_visualize = reshape(squeeze(eye_raw(:,:)),[128 128]); 
+
+
+% For better visualization and contrast of the eye images, histogram based
+% compensation will be done 
+
+std_within = 0.995; 
+% set maximum intensity to contain 99.5 % of intensity values per image
+[aa, val] = hist(eye_visualize(:),linspace(0,max(...
+                                    eye_visualize(:)),1000));
+    thresh = val(find(cumsum(aa)/sum(aa) > std_within,1,'first'));
+    
+% set threshold value to 65536
+eye_visualize = uint16(eye_visualize * 65536 / thresh); 
+
+close all
+figure(1); 
+imagesc(eye_visualize(:,:,1));
+axis image, 
+colormap gray;
+axis off
+
+figure(2); 
+imagesc(100*log(abs(s_L1)));
+
+figure(3); 
+imagesc(C);
+
+%%
+% Calculate MSE
+
+
+function v=shuffle(v)
      v=v(randperm(length(v)));
  end
 
@@ -104,95 +199,3 @@ load('MRI_datasets/Slice4/BadData/slice4_channel3.mat');
         end
     end
  end
-
- C= spiral_matrix(200,100,10,200, 5);
-close all
-figure(1); 
-imagesc(C);
-
-
-%%
-
-n = 512*128; % dimension of s
-p = round(n/10);
-kspace = reshape(slice4_channel1_goodData, n, 1);
-%C = sparse_matrix(p,n,p*500);
-%C = center_matrix(p,n);
-C = radial_matrix(p,n,30,50);
-%C = spiral_matrix(p,n,50,100000,15);
-y = C*kspace;
-
-close all
-axis image, 
-figure(1); 
-imagesc(C);
-
-%%
-% L1 minimum norm solution s_L1
-C = double(C);
-y = double(y);
-cvx_begin;
-    variable s_L1(n) complex;
-    minimize(norm(s_L1,1));
-    subject to 
-        norm(C*s_L1 - y, 2) < 10;
-cvx_end;
-
-%s_L2 = pinv(Theta)*y; % L2 minimum norm solution
-
-% Example of Steve Brunton code
-% https://stackoverflow.com/questions/66510827/compressed-sensing-why-does-my-convex-solver-fail-when-i-give-it-more-sample
-
-%%
-% IFFT of k-space data
-%channel 1 (replace "slice1_channel1_goodData" with
-%slice1_channel1_badData) for bad images
-s_L1 = reshape(s_L1, 512, 128)
-Data_img(:,:,1) = ifftshift(ifft2(s_L1),1);
-
-% clear compensation, preparation, based on fourier transformed blinked 
-% k-space data (Data_raw)
-clear_comp = linspace(10,0.1,size(Data_img,2)).^2; 
-clear_matrix = repmat(clear_comp,[size(Data_img,1) 1]);
-
-% combine 3 channels sum of squares and add clear compensation
-eye_raw  = sqrt( abs(squeeze(Data_img(:,:,1))).^2).* clear_matrix;  
-    
-% crop images because we are only interested in eye. Make it square 
-% 128 x 128
-crop_x = [128 + 60 : 348 - 33]; % crop coordinates
-eye_raw = eye_raw(crop_x, :);
-
-% Visualize the images. 
-
-%image
-eye_visualize = reshape(squeeze(eye_raw(:,:)),[128 128]); 
-
-
-% For better visualization and contrast of the eye images, histogram based
-% compensation will be done 
-
-std_within = 0.995; 
-% set maximum intensity to contain 99.5 % of intensity values per image
-[aa, val] = hist(eye_visualize(:),linspace(0,max(...
-                                    eye_visualize(:)),1000));
-    thresh = val(find(cumsum(aa)/sum(aa) > std_within,1,'first'));
-    
-% set threshold value to 65536
-eye_visualize = uint16(eye_visualize * 65536 / thresh); 
-
-close all
-figure(1); 
-imagesc(eye_visualize(:,:,1));
-axis image, 
-colormap gray;
-axis off
-
-figure(2); 
-imagesc(100*log(abs(s_L1)));
-
-figure(3); 
-imagesc(C);
-
-%%
-% Calculate MSE
